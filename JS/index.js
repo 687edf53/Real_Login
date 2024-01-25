e50d977929f3d8f5eca4ff3c41833f9bbd023f9b
687edf53 <sm3257698@gmail.com>
2024-01-25 22:49:27 +0200

    Upload Project

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  if (document.getElementById('username').value != '') {
+    var params = {
+      name: document.getElementById('username').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value
+    };
+    const serviceID = "service_u6cznmg";
+    const templateID = "template_e7bjdzm";
+    
+    emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById('username').value = "";
+      document.getElementById('email').value = "";
+      document.getElementById('message').value = "";
+      console.log(res);
+      alert('Your message sent successfully');
+    })
+    .catch(err => console.log(err));
+  } else {
+    alert('Please fill all the fields');
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# For Use Website
+
+go to:
+https://687edf53.github.io/Real_Login
